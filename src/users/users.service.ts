a57cc0effbfa124e2fa70f186ef1d055719f7b9d
Silas <dev.silas.freitas@outlook.com>
2024-08-15 17:34:04 -0300

    feat: add user functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,29 +2,61 @@ import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-interface UserToCreate {
-    name: string;
-    email: string;
-    password: string;
+interface UserToCreate extends Omit<User, 'id' | 'createdAt' | 'updatedAt'> {
+    confirmPassword: string;
 }
 
 @Injectable()
 export class UsersService {
-    constructor (private prismaService: PrismaService) {};
+    constructor(private prismaService: PrismaService) {}
 
-    async findByEmail(email: string): Promise<User | null> {
+    async findById(id: string): Promise<Omit<User, 'password'> | null> {
         const user = await this.prismaService.user.findUnique({
             where: {
-                email
-            }
+                id,
+            },
+            select: {
+                id: true,
+                fantasyName: true,
+                email: true,
+                cpf: true,
+                cnpj: true,
+                addressNumber: true,
+                companyName: true,
+                createdAt: true,
+                streetName: true,
+                updatedAt: true,
+                zipCode: true,
+            },
         });
 
         return user;
     }
 
-    async signIn(userData: UserToCreate): Promise<User | null> {
-        const user = await this.prismaService.user.create({ data: userData });
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
         return user;
     }
 
+    async create(data: UserToCreate): Promise<User> {
+        const user = await this.prismaService.user.create({
+            data: {
+                email: data.email,
+                password: data.password,
+                fantasyName: data.fantasyName,
+                cpf: data.cpf,
+                cnpj: data.cnpj,
+                addressNumber: data.addressNumber,
+                companyName: data.companyName,
+                zipCode: data.zipCode,
+            },
+        });
+
+        return user;
+    }
 }
