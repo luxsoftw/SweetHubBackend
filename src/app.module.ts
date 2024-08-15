7d3847c1fad2a765cedd862c2495ff51ea51408e
Silas <dev.silas.freitas@outlook.com>
2024-08-15 18:04:41 -0300

    fix: update app to new functions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SocketModule } from './socket/socket.module';
+import { AuthModule } from './auth/auth.module';
+import { HelpersModule } from './helpers/helpers.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { VerifyAuthMiddleware } from './verify-auth/verify-auth.middleware';
 
 @Module({
-  imports: [UsersModule, PrismaModule, SocketModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UsersModule,
+        PrismaModule,
+        SocketModule,
+        AuthModule,
+        HelpersModule,
+        DashboardModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(VerifyAuthMiddleware).forRoutes('dashboard');
+    }
+}
